f39e740f3452f785058dc1a083bc6fcc81d7a6a8
Yann Herklotz <git@yannherklotz.com>
2024-10-04 11:44:19 +0200

    Remove Basic.lean from DataflowRewriter.lean

--- DataflowRewriter.lean
+++ DataflowRewriter.lean
@@ -1,5 +1,4 @@
 -- This module serves as the root of the `DataflowRewriter` library.
 -- Import modules here that should be built as part of the library.
 
-import «DataflowRewriter».Basic
 import «DataflowRewriter».Module
