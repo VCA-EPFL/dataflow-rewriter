9794f64a4a2b3c361f91b1763a8873f40fa72f7f
Yann Herklotz <git@yannherklotz.com>
2024-10-27 16:54:01 +0100

    Add command line interface parser

--- Main.lean
+++ Main.lean
@@ -22,9 +22,45 @@ def dotToExprHigh (d : Parser.DotGraph) : Except String (ExprHigh String) := do
     ) (maps, [])
   return ⟨ maps'.instTypeMap.toList.toAssocList, conns ⟩
 
+structure CmdArgs where
+  outputFile : Option System.FilePath
+  inputFile : Option System.FilePath
+  help : Bool
+deriving Inhabited
+
+def CmdArgs.empty : CmdArgs := ⟨none, none, false⟩
+
+def parseArgs (args : List String) : Except String CmdArgs := go CmdArgs.empty args
+  where
+    go (c : CmdArgs) : List String → Except String CmdArgs
+    | .cons "-h" _rst | .cons "--help" _rst => .ok {c with help := true}
+    | .cons "-o" (.cons fp rst) | .cons "--output" (.cons fp rst) =>
+      go {c with outputFile := some fp} rst
+    | .cons fp rst => do
+      if "-".isPrefixOf fp then throw s!"argument '{fp}' not recognised"
+      if c.inputFile.isSome then throw s!"more than one input file passed"
+      go {c with inputFile := some fp} rst
+    | [] => do
+      if c.inputFile.isNone then throw s!"no input file passed"
+      return c
+
+def helpText : String :=
+  "dataflow-rewriter -- v0.1.0
+
+FORMAT
+  dataflow-rewriter [OPTIONS...] FILE
+
+OPTIONS
+  -h, --help         Print this help text
+  -o, --output FILE  Set output file
+"
+
 def main (args : List String) : IO Unit := do
-  let [filename] := args | throw <| IO.userError "Expecting one argument, the input file"
-  let fileContents ← IO.FS.readFile filename
+  let parsed ← IO.ofExcept <| parseArgs args
+  if parsed.help then
+    IO.print helpText
+    return
+  let fileContents ← IO.FS.readFile parsed.inputFile.get!
   let exprHigh ← IO.ofExcept do
     let dotGraph ← Parser.dotGraph.run fileContents
     dotToExprHigh dotGraph
