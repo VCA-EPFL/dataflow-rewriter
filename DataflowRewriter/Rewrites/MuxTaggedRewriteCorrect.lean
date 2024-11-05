c0d836758152f31671a9f8607f2f2365ed2e09ca
Martina <martina.camaioni@epfl.ch>
2024-11-05 16:51:33 +0100

    complete phi

--- DataflowRewriter/Rewrites/MuxTaggedRewriteCorrect.lean
+++ DataflowRewriter/Rewrites/MuxTaggedRewriteCorrect.lean
@@ -103,11 +103,42 @@ instance {Tag T} : MatchInterface (rhsModule Tag T) (lhsModule Tag T) where
 #reduce lhsNames
 #reduce lhsModuleType
 
+
 def φ {Tag T} (state_rhs : rhsModuleType Tag T) (state_lhs : lhsModuleType Tag T) : Prop :=
-  state_rhs.2.1.2.2 = state_lhs.2.2.2
+  state_rhs.2.2.1.2 ++ state_rhs.2.1.1.map Prod.snd  = state_lhs.2.1 ++ state_lhs.1.2 ∧
+  state_rhs.2.2.2.2 ++ state_rhs.2.1.2.1.map Prod.snd  = state_lhs.2.2.1 ++ state_lhs.1.2 ∧
+  state_rhs.2.1.2.2 = state_lhs.2.2.2 ∧
+  state_rhs.1 ++ state_rhs.2.2.1.1 ++ state_rhs.2.1.1.map Prod.fst = state_lhs.1.1 ∧
+  state_rhs.1 ++ state_rhs.2.2.2.1 ++ state_rhs.2.1.2.1.map Prod.fst = state_lhs.1.1
 
 theorem φ_indistinguishable {Tag T} :
-  ∀ x y, φ x y → Module.indistinguishable (rhsModule Tag T) (lhsModule Tag T) x y := sorry
+  ∀ x y, φ x y → Module.indistinguishable (rhsModule Tag T) (lhsModule Tag T) x y := by
+  unfold φ; intro x y H
+  constructor <;> intro ident new_i v Hcontains Hsem
+  . have Hkeys := AssocList.keysInMap Hcontains; clear Hcontains
+    fin_cases Hkeys
+    -- . simp [drunfold,drcompute]
+    --   have : decide (({ inst := InstIdent.top, name := "i_tag" } : InternalPort String) = { inst := InstIdent.top, name := "i_tag" }) = true := by decide
+
+    --   simp (config := {decide := true}) [this,drdecide]
+    . dsimp only [drunfold,seval,drcompute]
+      simp
+  · have Hkeys := keysInMap Hcontains; clear Hcontains
+    fin_cases Hkeys
+    let ⟨ ⟨ i, Ha, Hc ⟩, Hb ⟩ := Hsem; clear Hsem
+    let (x1, x2) := x; clear x
+    let (new_i1, new_i2) := new_i; clear new_i
+    subst_vars; dsimp at *
+    generalize h : x2[i] = y'
+    have Ht : ∃ (i : Fin x2.length), x2.get i = y' := by exists i
+    rw [← List.mem_iff_get] at Ht
+    have He := List.Perm.symm H
+    have Hiff := List.Perm.mem_iff (a := y') He
+    have Ht' : y' ∈ y := by rw [Hiff]; simp; cases Ht <;> tauto
+    rw [List.mem_iff_get] at Ht'
+    let ⟨ i', Hi' ⟩ := Ht'; clear Ht'
+    constructor; exists i'; and_intros; rfl
+    simp [←Hi']
 
 theorem correct_threeway_merge'' {Tag T: Type _} [DecidableEq T]:
   rhsModule Tag T ⊑_{φ} lhsModule Tag T := by
