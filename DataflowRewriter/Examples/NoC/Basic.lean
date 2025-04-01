5dd346cee0f8153038bebd1accddca77da949ce1
Gurvan Debaussart <gurvan@debauss.art>
2025-04-01 11:32:00 +0200

    Continue foundation for the NoC definition

--- DataflowRewriter/Examples/NoC/Basic.lean
+++ DataflowRewriter/Examples/NoC/Basic.lean
@@ -2,6 +2,8 @@
 -- Inputs are defined as a product between an arbitrary type T and a FlitHeader
 -- type, which gives information about the desired target of each message.
 
+-- FIXME: For now, there is a low of useless comments which should be removed
+
 import Lean
 import Init.Data.BitVec.Lemmas
 import Qq
@@ -28,27 +30,72 @@ variable (netsz : ℕ)    -- Network Size (Number of router)
 
 -- Types -----------------------------------------------------------------------
 
-def RouterID : Type :=
-  Fin netsz
+def RouterID (netsz : ℕ) : Type :=
+  -- FIXME: Fin netsz could be better, but it is annoying to use.
+  -- Notably below for the `nbranch` component,
+  -- we cannot easily use a List.range.
+  Nat
 
 structure FlitHeader : Type :=
   dest: RouterID netsz
--- TODO: Should this be deriving other stuff ?
+-- TODO: Should this be deriving stuff ?
 
 def Ident : Type :=
   String
+
+-- Components ------------------------------------------------------------------
+
+-- TODO: Do we need to have netsz in name ?
+@[drunfold]
+def nbranch' (name := "nbranch") : NatModule (List Data × List (RouterID netsz)) :=
+  { inputs :=
+    [
+      (0, ⟨
+        Data,
+        λ (oldValList, oldRouterIDList) val (newValList, newRouterIDList) =>
+          newValList = oldValList.concat val ∧ newRouterIDList = oldRouterIDList
+      ⟩),
+      (1, ⟨
+        RouterID netsz,
+        λ (oldValList, oldRouterIDList) b (newValList, newRouterIDList) =>
+          newValList = oldValList ∧ newRouterIDList = oldRouterIDList.concat b
+      ⟩),
+    ].toAssocList
+    outputs :=
+    -- TODO: We would like to have n be cast to a RouterID down the line
+      List.range netsz |>.map (λ n => Prod.mk ↑n
+        (⟨
+          Data,
+          λ (oldValList, oldRouterIDList) val (newValList, newRouterIDList) =>
+            val :: newValList = oldValList ∧
+            n :: newRouterIDList = oldRouterIDList
+        ⟩))
+      |>.toAssocList,
+  }
+
+@[drunfold]
+def nbranch :=
+  nbranch' Data netsz |>.stringify
 
 -- Environment -----------------------------------------------------------------
 
 def ε : IdentMap Ident (TModule1 Ident) :=
   [
+    -- TODO: Do we really require a merge ? Why would we ?
+    -- Merge might be necessary if we want to also give the FlitHeader to the
+    -- output flit ? Might make things harder for no reason, target shouldn't
+    -- care about header
     (s!"Merge {netsz}", ⟨_, StringModule.merge Data netsz⟩),
+
+    -- Splits are used to separate Data and FlitHeader
     -- TODO: Should we add Data and FlitHeader to Split Name ?
     ("Split",           ⟨_, StringModule.split Data (FlitHeader netsz)⟩),
+
+    -- Bags are used to receive message (One per router)
     ("Bag",             ⟨_, StringModule.bag Data⟩),
 
-    -- TODO: We actually need a Mux of nsz input, how could we get this?
-    -- Do we have to make it ourself?
+    -- Branching is used for routing
+    ("NBranch",         ⟨_, nbranch Data netsz⟩),
   ].toAssocList
 
 -- Implementation --------------------------------------------------------------
@@ -57,9 +104,10 @@ def ε : IdentMap Ident (TModule1 Ident) :=
 def nbag : ExprHigh Ident :=
   { modules := [].toAssocList, connections := [] }
 
--- TODO: Define a nmux component
+-- TODO: Define a NoC using, for each input, a split into a nbranch into each
+-- nbag
 
--- TODO: Define a NoC using nmux, nbag, split, merge, ...
--- Should the FlitHeader part be part of the received data?
+def noc : ExprHigh Ident :=
+  { modules := [].toAssocList, connections := [] }
 
 end DataflowRewriter.NoC
