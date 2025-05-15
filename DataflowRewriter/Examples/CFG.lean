a72f92c3daf2a564d5ef54b0a44320bef9a5f0f8
Yann Herklotz <git@yannherklotz.com>
2025-05-16 01:40:45 +0200

    Add CFG and Imp examples

---
+++ DataflowRewriter/Examples/CFG.lean
@@ -0,0 +1,110 @@
+/-
+Copyright (c) 2025 VCA Lab, EPFL. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yann Herklotz
+-/
+
+import DataflowRewriter.Module
+import DataflowRewriter.Simp
+import DataflowRewriter.ExprHigh
+import DataflowRewriter.AssocList.Basic
+import DataflowRewriter.TypeExpr
+import DataflowRewriter.Environment
+
+open Batteries (AssocList)
+
+namespace DataflowRewriter.CombModule
+
+@[simp] abbrev Val := Nat
+@[simp] abbrev Reg := Nat
+@[simp] abbrev Node := Nat
+
+structure Context where
+  memory : AssocList Reg Val
+  deriving Inhabited
+
+def update_loc (loc: Reg) (val: Val) (c: Context) : Context :=
+  ⟨c.memory.cons loc val⟩
+
+inductive Aexp where
+| lit : Val → Aexp
+| var : Reg → Aexp
+| add : Aexp → Aexp → Aexp
+
+inductive Bexp where
+| true : Bexp
+| false : Bexp
+| eq : Aexp → Aexp → Bexp
+| lt : Aexp → Aexp → Bexp
+| and : Bexp → Bexp → Bexp
+| or : Bexp → Bexp → Bexp
+| not : Bexp → Bexp
+
+inductive Instr where
+| skip : Node → Instr
+| assign : Reg → Aexp → Node → Instr
+| cond : Bexp → Node → Node → Instr
+| ret : Option Reg → Instr
+
+namespace Semantics
+
+inductive Aexp.sem : Context → Aexp → Val → Prop where
+  | lit {c n} : Aexp.sem c (Aexp.lit n) n
+  | var {c y r} : c.memory.find? r = some y → Aexp.sem c (Aexp.var r) y
+  | add {c a v b v'} : Aexp.sem c a v → Aexp.sem c b v' → Aexp.sem c (Aexp.add a b) (v + v')
+
+inductive Bexp.sem : Context → Bexp → Bool → Prop where
+  | true {c} : Bexp.sem c Bexp.true Bool.true
+  | false {c} : Bexp.sem c Bexp.false Bool.false
+  | eq {c a v b v'} : Aexp.sem c a v → Aexp.sem c b v' → Bexp.sem c (Bexp.eq a b) (v = v')
+  | lt {c a v b v'} : Aexp.sem c a v → Aexp.sem c b v' → Bexp.sem c (Bexp.lt a b) (v < v')
+  | and {c a v b v'} : Bexp.sem c a v → Bexp.sem c b v' → Bexp.sem c (Bexp.and a b) (v && v')
+  | or {c a v b v'} : Bexp.sem c a v → Bexp.sem c b v' → Bexp.sem c (Bexp.or a b) (v || v')
+  | not {c a v} : Bexp.sem c a v → Bexp.sem c (Bexp.not a) (¬ v)
+
+inductive State where
+| state
+  (pc : Node)
+  (env : AssocList Node Instr)
+  (context : Context) : State
+| ret
+  (v : Option Val) : State
+
+inductive Instr.sem : State → State → Prop where
+  | skip {c n pc env} :
+    env.find? pc = .some (Instr.skip n) →
+    Instr.sem (.state pc env c) (.state n env c)
+  | assign {c e v r n pc env} :
+    env.find? pc = .some (Instr.assign r e n) →
+    Aexp.sem c e v →
+    Instr.sem (.state pc env c) (.state n env (update_loc r v c))
+  | cond_true {c b t f env pc} :
+    env.find? pc = .some (Instr.cond b t f) →
+    Bexp.sem c b true →
+    Instr.sem (.state pc env c) (.state t env c)
+  | cond_false {c b t f env pc} :
+    env.find? pc = .some (Instr.cond b t f) →
+    Bexp.sem c b false →
+    Instr.sem (.state pc env c) (.state f env c)
+  | ret {pc env c v} :
+    env.find? pc = .some (Instr.ret v) →
+    Instr.sem (.state pc env c) (.ret v)
+
+end Semantics
+
+namespace Denotation
+
+def moduleGen (f : Context → Option Context) : StringModule (Option Context) :=
+  NatModule.stringify {
+    inputs := [(0, ⟨ Context, fun
+      | .none, s, .some c' => f s = .some c'
+      | _, _, _ => False ⟩)].toAssocList,
+    outputs := [(0, ⟨ Context, fun
+      | .some c, s, .none => s = c
+      | _, _, _ => False ⟩)].toAssocList,
+    init_state := λ s => s = default
+  }
+
+end Denotation
+
+end DataflowRewriter.CombModule
---
+++ DataflowRewriter/Examples/Imp.lean
@@ -0,0 +1,107 @@
+/-
+Copyright (c) 2025 VCA Lab, EPFL. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yann Herklotz
+-/
+
+import DataflowRewriter.Module
+import DataflowRewriter.Simp
+import DataflowRewriter.ExprHigh
+import DataflowRewriter.AssocList.Basic
+import DataflowRewriter.TypeExpr
+import DataflowRewriter.Environment
+
+open Batteries (AssocList)
+
+namespace DataflowRewriter.CombModule
+
+abbrev Val := Nat
+abbrev Reg := Nat
+
+structure Context where
+  memory : AssocList Reg Val
+  deriving Inhabited
+
+def update_loc (loc: Reg) (val: Val) (c: Context) : Context :=
+  ⟨c.memory.cons loc val⟩
+
+inductive Aexp where
+  | lit : Val → Aexp
+  | var : Reg → Aexp
+  | add : Aexp → Aexp → Aexp
+
+inductive Bexp where
+  | true : Bexp
+  | false : Bexp
+  | eq : Aexp → Aexp → Bexp
+  | lt : Aexp → Aexp → Bexp
+  | and : Bexp → Bexp → Bexp
+  | or : Bexp → Bexp → Bexp
+  | not : Bexp → Bexp
+
+inductive Com where
+  | skip : Com
+  | assign : Reg → Aexp → Com
+  | seq : Com → Com → Com
+  | cond : Bexp → Com → Com → Com
+  | while : Bexp → Com → Com
+  | assert : Bexp → Com
+
+namespace Semantics
+
+inductive Aexp.sem : Context → Aexp → Val → Prop where
+  | lit {c n} : Aexp.sem c (Aexp.lit n) n
+  | var {c y r} : c.memory.find? r = some y → Aexp.sem c (Aexp.var r) y
+  | add {c a v b v'} : Aexp.sem c a v → Aexp.sem c b v' → Aexp.sem c (Aexp.add a b) (v + v')
+
+inductive Bexp.sem : Context → Bexp → Bool → Prop where
+  | true {c} : Bexp.sem c Bexp.true Bool.true
+  | false {c} : Bexp.sem c Bexp.false Bool.false
+  | eq {c a v b v'} : Aexp.sem c a v → Aexp.sem c b v' → Bexp.sem c (Bexp.eq a b) (v = v')
+  | lt {c a v b v'} : Aexp.sem c a v → Aexp.sem c b v' → Bexp.sem c (Bexp.lt a b) (v < v')
+  | and {c a v b v'} : Bexp.sem c a v → Bexp.sem c b v' → Bexp.sem c (Bexp.and a b) (v && v')
+  | or {c a v b v'} : Bexp.sem c a v → Bexp.sem c b v' → Bexp.sem c (Bexp.or a b) (v || v')
+  | not {c a v} : Bexp.sem c a v → Bexp.sem c (Bexp.not a) (¬ v)
+
+structure State where
+  context : Context
+  command : Com
+
+inductive Com.sem : Context → Com → Context → Prop where
+  | skip {c} : Com.sem c Com.skip c
+  | assign {c e v r} : Aexp.sem c e v → Com.sem c (Com.assign r e) (update_loc r v c)
+  | seq {c s1 c' s2 c''} : Com.sem c s1 c' → Com.sem c' s2 c'' → Com.sem c (Com.seq s1 s2) c''
+  | cond_true {c b t c' f} : Bexp.sem c b True → Com.sem c t c' → Com.sem c (Com.cond b t f) c'
+  | cond_false {c b t c' f} : Bexp.sem c b False → Com.sem c f c' → Com.sem c (Com.cond b t f) c'
+  | while_false {c b a} : Bexp.sem c b False → Com.sem c (Com.while b a) c
+  | while_true {c b a c'} : Bexp.sem c b True → Com.sem c (Com.seq a (Com.while b a)) c' → Com.sem c (Com.while b a) c'
+  | assert_true {c b} : Bexp.sem c b True → Com.sem c (Com.assert b) c
+
+end Semantics
+
+namespace Denotation
+
+def moduleGen (f : Context → Option Context) : StringModule (Option Context) :=
+  NatModule.stringify {
+    inputs := [(0, ⟨ Context, fun
+      | .none, s, .some c' => f s = .some c'
+      | _, _, _ => False ⟩)].toAssocList,
+    outputs := [(0, ⟨ Context, fun
+      | .some c, s, .none => s = c
+      | _, _, _ => False ⟩)].toAssocList,
+    init_state := λ s => s = default
+  }
+
+-- def Com.toGraph : Com → ExprLow String
+-- | skip c => .base ⟨[(0, 0)].toAssocList, [(0, 0)].toAssocList⟩ "Skip"
+-- | assign r a => .base ⟨[(0, 0)].toAssocList, [(0, 0)].toAssocList⟩ "Assign"
+-- | seq s1 s2 =>
+-- | cond_true {c b t c' f} : Bexp.sem c b True → Com.sem c t c' → Com.sem c (Com.cond b t f) c'
+-- | cond_false {c b t c' f} : Bexp.sem c b False → Com.sem c f c' → Com.sem c (Com.cond b t f) c'
+-- | while_false {c b a} : Bexp.sem c b False → Com.sem c (Com.while b a) c
+-- | while_true {c b a c'} : Bexp.sem c b True → Com.sem c (Com.seq a (Com.while b a)) c' → Com.sem c (Com.while b a) c'
+-- | assert b => .base ⟨[(0, 0)].toAssocList, [(0, 0)].toAssocList⟩ "Assert"
+
+end Denotation
+
+end DataflowRewriter.CombModule
